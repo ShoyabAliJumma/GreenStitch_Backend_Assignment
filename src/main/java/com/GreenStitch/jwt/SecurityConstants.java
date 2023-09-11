20efb897787ddf3da94c4279f988e25e89c6d49c
ShoyabAliJumma <shoyabalipathan@gmail.com>
2023-09-11 12:12:34 +0530

    GreenStitch_Backend_Assignment

---
+++ src/main/java/com/GreenStitch/jwt/SecurityConstants.java
@@ -0,0 +1,7 @@
+package com.GreenStitch.jwt;
+
+public interface SecurityConstants {
+
+    public static final String JWT_KEY ="shoyabalijumma";
+    public static final String JWT_HEADER ="Authorization";
+}
